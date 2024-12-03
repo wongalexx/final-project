96616f31204a47e4a3ac6bf576a1ae75be9cb899
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-12-03 12:06:35 -0800

    rough backend implementation for quizzes, need to account for questions in future

---
+++ kanbas-node-server-app/Kanbas/Quizzes/dao.js
@@ -0,0 +1,19 @@
+import model from "./model.js";
+export function findQuizzesForCourse(courseId) {
+  return model.find({ course: courseId });
+}
+
+export function createQuiz(quiz) {
+  delete quiz._id;
+  return model.create(quiz);
+}
+
+export function deleteQuiz(quizId) {
+  return model.deleteOne({ _id: quizId });
+}
+
+export function updateQuiz(quizId, quizUpdates) {
+  return model.updateOne({ _id: quizId }, quizUpdates);
+}
+
+// DAO functions for questions too
---
+++ kanbas-node-server-app/Kanbas/Quizzes/schema.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+  title: String,
+  points: Number,
+  question: String,
+  type: {
+    type: String,
+    enum: ["Multiple Choice", "True/False", "Fill in the Blank"],
+    required: true,
+  },
+  choices: [
+    {
+      text: String,
+      correct: Boolean,
+    },
+  ],
+  correctAnswer: String, // Not sure
+});
+
+const quizSchema = new mongoose.Schema(
+  {
+    title: String,
+    course: String,
+    quizType: {
+      type: String,
+      enum: [
+        "Graded Quiz",
+        "Practice Quiz",
+        "Graded Survey",
+        "Ungraded Survey",
+      ],
+      default: "Graded Quiz",
+    },
+    points: Number,
+    assignmentGroup: {
+      type: String,
+      enum: ["Quizzes", "Exams", "Assignments", "Project"],
+      default: "Quizzes",
+    },
+    shuffleAnswers: { type: Boolean, default: true },
+    timeLimit: { type: Number, default: 20 },
+    multipleAttempts: { type: Boolean, default: false },
+    attemptsAllowed: {
+      type: Number,
+      default: 1,
+    },
+    showCorrectAnswers: {
+      type: String,
+      enum: ["Never", "Immediately", "After Due Date"],
+      default: "Never",
+    },
+    accessCode: { type: String, default: "" },
+    oneQuestionAtATime: { type: Boolean, default: true },
+    webcamRequired: { type: Boolean, default: false },
+    lockQuestionsAfterAnswering: { type: Boolean, default: false },
+    availableFromDate: String,
+    availableUntilDate: String,
+    due: String,
+    questions: [questionSchema],
+  },
+  { collection: "quizzes" }
+);
+
+export default quizSchema;
